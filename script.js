ad6a6adfd8627a769fdf78157177f2dead3efcb1
Andre Pan <andre.pan.coding@gmail.com>
2018-08-27 17:37:45 -0700

    page animate on change

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ function DisplayPage (navItem){
     var targetPageID = ExtractPageIDByNavItem(navItem);
     var pageData = document.getElementById("PageData");
     var currentPageID = pageData.getAttribute("data-CurrentPageID");
+    if (targetPageID != currentPageID){
     pageData.setAttribute("data-CurrentPageID", targetPageID);
 
-    document.getElementById(currentPageID).style.display = "none";
-    document.getElementById(targetPageID).style.display = "block";
+    document.getElementById(currentPageID).classList.add("animate-remove");
+    document.getElementById(targetPageID).classList.remove("animate-remove");
 
+    setTimeout(function(){SetDisplayValue(currentPageID, "none");}, 1000);
+    SetDisplayValue(targetPageID, "block");
+    }
 }
+
+function SetDisplayValue (elementID, displayValue){
+    document.getElementById(elementID).style.display = displayValue;
+}
+
 
 function DisplayPageMobile (navItem){
     DisplayPage(navItem);
