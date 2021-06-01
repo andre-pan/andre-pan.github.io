984cf05bcd8d43534b2cb0c3b1eb08f9970d9ed7
andre-pan <30707580+andre-pan@users.noreply.github.com>
2021-06-01 00:15:02 -0700

    home text + font family

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 function HomePage(){
     return (
-        <div>
-            Home Page
+        <div className="text-center">
+            <h1>
+                Andre Pan / Software Developer
+                <br/>
+                Computer Science / Mathematics B.S.
+                <br/>
+                UC San Diego
+            </h1>
         </div>
     );
 }
