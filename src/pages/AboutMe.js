26cf4e57efc230ac2838c8ba18cad085dd88b1d4
andre-pan <30707580+andre-pan@users.noreply.github.com>
2021-06-25 18:49:53 -0700

    add profile image to about me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ProfileImgSrc from '../assets/ProfileImg.png';
 
 function AboutMePage(){
     return (
-        <div>
-            About Me Page
+        <div className="flex-center">
+            <img src={ProfileImgSrc} className="responsive-img profile-img"/>
         </div>
     );
 }
